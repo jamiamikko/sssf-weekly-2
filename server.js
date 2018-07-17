75f801d1c65a4e593c8a37ad5b7ca79e85b5567b
jamiamikko <jamiamikko@gmail.com>
2018-07-17 18:38:16 +0300

    First commit

---
+++ server.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const path = require('path');
+const sharp = require('sharp');
+const fs = require('fs');
+
+const storage = multer.diskStorage({
+  destination: 'public/uploads/',
+  filename: (req, file, callback) => {
+    callback(
+      null,
+      file.fieldname + '-' + Date.now() + path.extname(file.originalname)
+    );
+  }
+});
+
+const upload = multer({storage: storage}).single('image');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/', express.static('public'));
+
+const convertImage = (file, height, width) =>
+  new Promise((resolve, reject) => {
+    const newName =
+      file.destination +
+      file.fieldname +
+      '_' +
+      height +
+      'x' +
+      width +
+      '-' +
+      Date.now() +
+      path.extname(file.originalname);
+
+    sharp(file.path)
+      .resize(height, width)
+      .toFile(newName, (err, info) => {
+        if (err) reject(err);
+        else resolve(newName);
+      });
+  });
+
+app.post('/upload', upload, (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+  
+  if (!req.body || !req.file) sendStatus(400);
+  const date = new Date()
+    .toISOString()
+    .replace(/T/, ' ')
+    .replace(/\..+/, '');
+
+  const dataObj = {
+    id: Math.floor(Math.random() * 100 + 1),
+    time: date,
+    category: req.body.category,
+    title: req.body.title,
+    details: req.body.description,
+    coordinates: {
+      lat: parseFloat(req.body.latitude),
+      lng: parseFloat(req.body.longitude)
+    },
+    original: req.file.path.replace('public/', '')
+  };
+
+  convertImage(req.file, 320, 300)
+    .then((data) => {
+      dataObj.thumbnail = data.replace('public/', '');
+
+      convertImage(req.file, 768, 720)
+        .then((response) => {
+          dataObj.image = response.replace('public/', '');
+
+          fs.readFile('./public/data.json', (err, data) => {
+            const json = JSON.parse(data);
+
+            json.push(dataObj);
+
+            fs.writeFile('./public/data.json', JSON.stringify(json));
+            res.sendStatus(200);
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          sendStatus(400);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      sendStatus(400);
+    });
+});
+
+app.listen(port, () => console.log('Listening to port 3000'));
